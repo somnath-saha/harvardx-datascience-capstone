4d8ae8ecb1c7aa52b69df426bfc866213ee92a93
Somnath Saha <sosaha@adobe.com>
2021-07-28 01:06:07 +0530

    update code and add rmd file

--- cyo-housesale-project/code.R
+++ cyo-housesale-project/code.R
@@ -2,6 +2,29 @@
 # This dataset contains house sale prices for King County, which includes Seattle. 
 # It includes homes sold between May 2014 and May 2015.
 # 
+# Details of the columns
+# id: house id
+# date: date that the house was bought.
+# price: house price
+# bedrooms: number of bedroom
+# bathrooms: number of bathroom
+# sqft_living: Square foot Living
+# sqft_lot: Square foot Lot
+# floors: Number of floor
+# waterfront: waterfront
+# view: Number of view
+# condition: condition
+# grade: grade
+# sqft_above: Square foot above
+# sqft_basement: Square foot basement
+# yr_built: year that house was built
+# yr_renovated: year that house was renovated
+# zipcode: zipcode
+# lat: latitude
+# long: longitude
+# sqft_living15: Square foot Living in 2015
+# sqft_lot15: Square foot Lot in 2015
+
 
 library(tidyverse)
 
@@ -41,20 +64,21 @@ housedata %>% filter(sqft_lot < 100000) %>%
    labs(x = "sqft_lot (In 1000 sqft)", y = "Count", caption = caption_text) + plot_theme
 
 # Distribution of number of houses based on the following column values - 
-# 'floors', 'waterfront', 'view', 'condition', 'grade', 'yr_built', 'yr_renovated', 'zipcode'
+# 'floors', 'waterfront', 'view', 'condition', 'grade'
 grp_col_names <- c('floors', 'waterfront', 'view', 'condition', 'grade')
-
 lapply(grp_col_names, function(c) {
    res <- housedata %>% group_by(.dots = c) %>%  
       summarise('Count' = n(), Percentage = n() * 100 / nrow(housedata)) %>%
-      arrange(desc(Percentage)) %>% top_n(10)
+      arrange(desc(Percentage))
    res %>% ggplot(aes_string(x=c, y='Count')) + 
       geom_bar(stat = "identity", color = I("white"), fill = "skyblue") +
-      geom_text(aes_string(label='Count'), position=position_dodge(width=0.5), vjust=-0.3) +
+      geom_text(aes_string(label='Count'), position=position_dodge(width=0.75), vjust=-0.3) +
       labs(x = c, y = "Count", caption = caption_text) + plot_theme
 })
 
-grp_col_names_yr <- c('yr_built', 'yr_renovated', 'zipcode')
+# Distribution of number of houses based on the following column values - 
+# 'yr_built', 'yr_renovated'
+grp_col_names_yr <- c('yr_built', 'yr_renovated')
 lapply(grp_col_names_yr, function(c) {
    res <- housedata %>% group_by(.dots = c) %>%  
       summarise('Count' = n(), Percentage = n() * 100 / nrow(housedata)) %>%
@@ -91,29 +115,76 @@ housedata %>% filter(bedrooms < 15) %>% ggplot(aes(x=sqft_lot, y=price, group=sq
    scale_x_continuous(breaks = seq(0, 15, 1)) +
    scale_y_continuous(breaks = seq(0, 8000000, 1000000), labels = sprintf("%sM", seq(0, 8, 1))) +
    labs(x = "No of Bedrooms", y = "Price of House ($)", caption = caption_text) + plot_theme
-# boxplot(price ~ bedrooms, data = housedata, 
-#          xlab = "No of Bedrooms",
-#          ylab = "Price of House", 
-#          col = "maroon",
-#          #ylim = sprintf("%sM", seq(0, 8, 0.5)),
-#          #names = sprintf("%sM", seq(0, 8, 0.5)),
-#          main = "Bedrooms vs Price of House"
-#          )
 
-housedata %>% group_by(gr = cut(price/1000000, breaks= seq(0, 10, by = 1)) ) %>% 
-               summarise(n = n()) %>%
-               arrange(desc(as.numeric(n)))
+#Predicting values of price based on other available parameters
+#models <- c("glm", "lda", "naive_bayes", "svmLinear", "knn", "gamLoess", "multinom", "qda", "rf", "adaboost")
+#lm, svmLinear
 
-housedata %>% group_by(gr = cut(price/1000, breaks= seq(0, 20000, by = 100)) ) %>% 
-               summarise(n = n()) %>%
-               arrange(desc(as.numeric(n)))
+if(!require(caret)) install.packages("caret", repos = "http://cran.us.r-project.org")
+if(!require(xgboost)) install.packages("caret", repos = "http://cran.us.r-project.org")
+if(!require(xgboost)) install.packages("caret", repos = "http://cran.us.r-project.org")
+library(caret)
+library(dslabs)
 
-housedata %>% qplot(price, )
+hdata <- housedata %>% select(-id, -date, -sqft_living15, -sqft_lot15)
+test_index <- createDataPartition(y = hdata$price, times = 1, p = 0.1, list = FALSE)
+hdataTemp <- hdata[-test_index,]
+validation <- hdata[test_index,]
+
+test_index <- createDataPartition(y = hdataTemp$price, times = 1, p = 0.1, list = FALSE)
+hdata_train <- hdataTemp[-test_index,]
+hdata_test <- hdataTemp[test_index,]
+
+rm(hdata, hdataTemp)
+
+models <- c("glm", "", "", "svmLinear", "knn", "gamLoess", "multinom", "qda", "rf", "adaboost")
+
+models2 <- c("xgbDART", "xgbLinear", "xgbTree", "elm", "neuralnet", "nnet", "pcaNNet")
+
+hdata_test <- hdata_test %>% select(price, bedrooms, bathrooms, sqft_living, sqft_basement, sqft_above)
+validation <- validation %>% select(price, bedrooms, bathrooms, sqft_living, sqft_basement, sqft_above)
+
+fits <- lapply(models2, function(model){ 
+   print(model)
+   train(price ~ ., method = model, data = hdata_test)
+}) 
+pred <- sapply(fits, function(object) predict(object, newdata = validation))
+
+fit1 <- train(price ~ ., method = "lm", data = hdata_test)
+fit1 <- train(price ~ ., method = "svmLinear", data = hdata_test)
+fit1 <- train(price ~ ., method = "knn", data = hdata_test)
+fit1 <- train(price ~ ., method = "gamLoess", data = hdata_test)
+fit1 <- train(price ~ ., method = "multinom", data = hdata_test)
+fit1 <- train(price ~ ., method = "rf", data = hdata_test)
+fit1 <- train(price ~ ., method = "glm", data = hdata_test)
+fit1 <- train(price ~ ., method = "gbm", data = hdata_test)
+
+fit1 <- train(price ~ ., method = "xgbDART", data = hdata_test)
+fit1 <- train(price ~ ., method = "xgbLinear", data = hdata_test)
+fit1 <- train(price ~ ., method = "xgbTree", data = hdata_test)
+fit1 <- train(price ~ ., method = "elm", data = hdata_test)
+fit1 <- train(price ~ ., method = "neuralnet", data = hdata_test)
+fit1 <- train(price ~ ., method = "nnet", data = hdata_test)
+fit1 <- train(price ~ ., method = "pcaNNet", data = hdata_test)
 
 
+pred1 <- predict(fit1, newdata = validation)
+RMSE(pred1, validation$price)
+acc1
 
-Reference :
-   https://www.tutorialspoint.com/r/r_boxplots.htm
+df <- data.frame(pred1, validation$price)
+
+names(fits) <- models
+pred <- sapply(fits, function(object) predict(object, newdata = validation))
+
+dim(pred)
+
+acc <- colMeans(pred == mnist_27$test$y)
+acc
+mean(acc)
+
+#Reference :
+#   https://www.tutorialspoint.com/r/r_boxplots.htm
 
 
 
